0e1617c727506ce4eb24012cb2d63cea5ba941c6
Piotr Persona <persona.piotr@gmail.com>
2019-04-16 18:25:23 +0200

    Define basic schema
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- schema/data_schema.go
+++ schema/data_schema.go
@@ -2,31 +2,54 @@ package schema
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-type HttpCode struct {
+// HTTPCode describes particular http status code.
+type HTTPCode struct {
 	Code        string `json:"code"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
-type HttpGroup struct {
+// HTTPGroup describes particular http group
+// There are 5 types of groups:
+// 1xx - Informational
+// 2xx - Success
+// 3xx - Redirection
+// 4xx - Client Error
+// 5xx - Server Error
+type HTTPGroup struct {
 	Prefix      string `json:"prefix"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
-type HttpData struct {
-	Groups []HttpGroup `json:"groups"`
-	Codes  []HttpCode  `json:"codes"`
+// HTTPData contains set of all available Http codes and groups.
+type HTTPData struct {
+	Groups []HTTPGroup `json:"groups"`
+	Codes  []HTTPCode  `json:"codes"`
 }
 
-func (hd *HttpData) Dump(filename string) {
+// Dump will marshal data to json file.
+func (hd *HTTPData) Dump(filename string) {
 	jsonBytes, err := json.Marshal(hd)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ioutil.WriteFile(filename, jsonBytes, 0644)
+}
+
+// RetrieveCode will fetch information about particular code.
+func (hd *HTTPData) RetrieveCode(code string) (hc HTTPCode, err error) {
+	for _, httpCode := range hd.Codes {
+		if httpCode.Code == code {
+			hc = httpCode
+			return
+		}
+	}
+	err = fmt.Errorf("No such code: %s", code)
+	return
 }
